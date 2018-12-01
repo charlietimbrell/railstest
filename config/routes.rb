3f50cd4256a890f1c722b27d0fed26f02ff7aef0
Charlotte Timbrell <charlottetimbrell@Charlottes-MacBook-Pro.local>
2018-12-01 17:07:55 +0000

    rails test

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/info'
+  root to: redirect('pages/home')
+  resources :ideas
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
